f1adcdc85ba88564cce984977f3ef0f0d171b541
Yushchuk Dmytro <Dreuk03@gmail.com>
2022-07-12 23:47:00 +0300

    Add fetch API in project

--- js/script.js
+++ js/script.js
@@ -240,82 +240,77 @@ window.addEventListener('DOMContentLoaded', () => {
 	).rendor();
 
 	// Forms
-const forms = document.querySelectorAll('form');
+	const forms = document.querySelectorAll('form');
 
-const messages = {
-	loading: 'img/form/spinner.svg',
-	success: 'Ok!Go go go!',
-	faild: 'Something wrong...'
-};
+	const messages = {
+		loading: 'img/form/spinner.svg',
+		success: 'Ok!Go go go!',
+		faild: 'Something wrong...'
+	};
 
-forms.forEach(item => {
-	formData(item);
-});
+	forms.forEach(item => {
+		formData(item);
+	});
 
-function formData(form) {
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
+	function formData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
 
-	const statusDate = document.createElement('img');
-	statusDate.src = messages.loading;
-	statusDate.style.cssText = `
+			const statusDate = document.createElement('img');
+			statusDate.src = messages.loading;
+			statusDate.style.cssText = `
 		display: block;
 		margin: 0 auto;
 	`;
-	form.insertAdjacentElement('afterend', statusDate);
+			form.insertAdjacentElement('afterend', statusDate);
 
+			const formData = new FormData(form);
 
-	const request = new XMLHttpRequest();
-	request.open('POST', 'server.php');
-	request.setRequestHeader('Content-type', 'application/json');
+			const obj = {};
+			formData.forEach(function (value, key) {
+				obj[key] = value;
+			});
 
+			fetch('server.php', {
+					method: 'POST',
+					headers: {
+						'Content-type': 'aplication/json'
+					},
+					body: JSON.stringify(obj)
+				})
+				.then(data => data.text())
+				.then(data => {
+					console.log(data);
+					showThanksModal(messages.success);
+					statusDate.remove();
+				}).catch(() => {
+					showThanksModal(messages.faild);
+				}).finally(() => {
+					form.reset();
+				});
+		});
+	}
 
-const formData = new FormData(form);
+	function showThanksModal(message) {
+		const prevModalDialog = document.querySelector('.modal__dialog');
+		prevModalDialog.classList.add('hide');
+		openModal();
 
-const obj = {};
-
-formData.forEach(function(value, key) {
-		obj[key] = value;
-});
-
-const json = JSON.stringify(obj);
-
-	request.send(json);
-	request.addEventListener('load', () => {
-		if(request.status === 200) {
-			console.log(request.response);
-			 showThanksModal(messages.success);
-			form.reset();
-		statusDate.remove();
-		} else {
-			showThanksModal(messages.faild);
-		}
-	});
-});
-}
-
-function showThanksModal(message) {
-	const prevModalDialog = document.querySelector('.modal__dialog');
-	prevModalDialog.classList.add('hide');
-	openModal();
-
-	const thanksModal = document.createElement('div');
-	thanksModal.classList.add('modal__dialog');
-	thanksModal.innerHTML = `
+		const thanksModal = document.createElement('div');
+		thanksModal.classList.add('modal__dialog');
+		thanksModal.innerHTML = `
 	<div class="modal__content">
 		<div data-close class="modal__close">&times;</div>
 		<div class="modal__title">${message}</div>
 	</div>
 	`;
 
-	document.querySelector('.modal').append(thanksModal);
-	setTimeout(() => {
+		document.querySelector('.modal').append(thanksModal);
+		setTimeout(() => {
 			thanksModal.remove();
 			prevModalDialog.classList.add('show');
 			prevModalDialog.classList.remove('hide');
 			closeModal();
-	}, 4000);
-
-}
-
+		}, 4000);
+	}
 });
