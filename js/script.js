abad00ca2d58a3a46c9ae90ff16ab92511cca480
Yushchuk Dmytro <Dreuk03@gmail.com>
2022-07-16 21:12:33 +0300

    Lesson №89.JSON-server

--- js/script.js
+++ js/script.js
@@ -313,4 +313,8 @@ window.addEventListener('DOMContentLoaded', () => {
 			closeModal();
 		}, 4000);
 	}
+	fetch(' http://localhost:3000/menu')
+	.then(data => data.json())
+	.then(res => console.log(res));
 });
+
