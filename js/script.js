a26c653837b6277c1fc11e987c9b051055db3dac
Yushchuk Dmytro <Dreuk03@gmail.com>
2022-07-17 22:27:37 +0300

    Lesson 93.Create slider part 2

--- js/script.js
+++ js/script.js
@@ -310,47 +310,114 @@ window.addEventListener('DOMContentLoaded', () => {
 		prev = document.querySelector('.offer__slider-prev'),
 		next = document.querySelector('.offer__slider-next'),
 		total = document.querySelector('#total'),
-		current = document.querySelector('#current');
+		current = document.querySelector('#current'),
+		slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+		slideFild = document.querySelector('.offer__slider-inner'),
+		width = window.getComputedStyle(slidesWrapper).width;
 
 	let slideIndex = 1;
-
-	showSlide(slideIndex);
-
-	if (slides.length < 10) {
-		total.textContent = `0${slides.length}`;
-	} else {
-		total.textContent = slides.length;
-	}
-
-	function showSlide(n) {
-		if (n > slides.length) {
-			slideIndex = 1;
-		}
-
-		if (n < 1) {
-			slideIndex = slides.length;
-		}
-
-		slides.forEach(item => item.style.display = 'none');
-
-		slides[slideIndex - 1].style.display = 'block';
+	let offset = 0;
 
 		if (slides.length < 10) {
-			current.textContent = `0${slideIndex}`;
+		total.textContent = `0${slides.length}`;
+		current.textContent = `0${slideIndex}`;
+	} else {
+		total.textContent = slides.length;
+		current.textContent = slideIndex;
+	}
+
+
+	slideFild.style.width = 100 * slides.length + '%';
+	slideFild.style.display = 'flex';
+	slideFild.style.transition ='0.5s all';
+
+	slidesWrapper.style.overflow = 'hidden';
+
+	slides.forEach(slide => {
+		slide.style.width = width;
+	});
+
+next.addEventListener('click', () => {
+if(offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+		offset = 0;
+	} else {
+		offset += +width.slice(0, width.length - 2);
+	}
+
+	slideFild.style.transform = `translateX(-${offset}px)`;
+
+	if (slideIndex == slides.length) {
+
+		slideIndex = 1;
 		} else {
-			current.textContent = slideIndex;
+			slideIndex ++;
 		}
+		if(slides.length < 10) {
+			current.textContent  = `0${slideIndex}`;
+		} else {
+			current.textContent  = slideIndex;
+		}
+});
+
+prev.addEventListener('click', () => {
+	if(offset == 0) {
+		offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+	} else {
+		offset -= +width.slice(0, width.length - 2);
 	}
 
-	function plusSlide(n) {
-		showSlide(slideIndex += n);
-	}
+	slideFild.style.transform = `translateX(-${offset}px)`;
 
-	next.addEventListener('click', () => {
-		plusSlide(1);
-	});
+	if (slideIndex == 1) {
+		slideIndex = slides.length;
+		
+		} else {
+			slideIndex --;
+		}
+		if(slides.length < 10) {
+			current.textContent  = `0${slideIndex}`;
+		} else {
+			current.textContent  = slideIndex;
+		}
+});
 
-	prev.addEventListener('click', () => {
-		plusSlide(-1);
-	});
+	// showSlide(slideIndex);
+
+	// if (slides.length < 10) {
+	// 	total.textContent = `0${slides.length}`;
+	// } else {
+	// 	total.textContent = slides.length;
+	// }
+
+	// function showSlide(n) {
+	// 	if (n > slides.length) {
+	// 		slideIndex = 1;
+	// 	}
+
+	// 	if (n < 1) {
+	// 		slideIndex = slides.length;
+	// 	}
+
+	// 	slides.forEach(item => item.style.display = 'none');
+
+	// 	slides[slideIndex - 1].style.display = 'block';
+
+	// 	if (slides.length < 10) {
+	// 		current.textContent = `0${slideIndex}`;
+	// 	} else {
+	// 		current.textContent = slideIndex;
+	// 	}
+	// }
+
+	// function plusSlide(n) {
+	// 	showSlide(slideIndex += n);
+	// }
+
+	// next.addEventListener('click', () => {
+	// 	plusSlide(1);
+	// });
+
+	// prev.addEventListener('click', () => {
+	// 	plusSlide(-1);
+	// });
 });
